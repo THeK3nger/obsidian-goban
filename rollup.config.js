87197e432f0a07ac0eb44670d44af52cb02f4530
Davide Aversa <thek3nger@gmail.com>
2026-05-31 15:29:34 +0200

    chore: bump third party dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -2,20 +2,22 @@ import typescript from "@rollup/plugin-typescript";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
+const isProd = process.env.BUILD === "production";
+
 export default {
   input: "./src/main.ts",
   output: {
     dir: ".",
+    entryFileNames: "main.js",
     sourcemap: "inline",
+    sourcemapExcludeSources: isProd,
     format: "cjs",
     exports: "default",
   },
   external: ["obsidian"],
   plugins: [
-    typescript({
-      tsconfig: "./tsconfig.json"
-    }),
+    typescript({ allowSyntheticDefaultImports: true, outDir: "./dist" }),
     nodeResolve({ browser: true }),
-    commonjs(),
+    commonjs({ include: "node_modules/**" }),
   ],
 };
